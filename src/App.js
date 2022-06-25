68f15f587a5322b2d2812619c0fcd4cf9d02bf74
Blake Hatch <blakewihatch@gmail.com>
2022-06-25 14:09:09 -0400

    Got the 3d components working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './Components/Title'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </header>
+      <Title/>
     </div>
   );
 }
